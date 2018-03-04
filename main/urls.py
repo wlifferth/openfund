aadd84d778a8769f0c1313373e2f74aa5bc019f3
wlifferth <wlifferth@icloud.comm>
2018-03-03 23:23:36 -0500

    Created main build-profile functionality

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.splash, name='splash'),
-    path('/login', views.login, name='login'),
-    path('/signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('build-profile/<user_id>', views.build_profile, name="build-profile"),
+    path('user/<user_id>/select-strategy', views.select_strategy, name="select-strategy"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -11,5 +11,25 @@ def login(request):
     return HttpResponse("You've reached login")
 
 def signup(request):
-    context = {}
-    return HttpResponse("You've reacached signup")
+    if request.method == "GET":
+        context = {}
+        return render(request, 'main/signup.html', context)
+    elif request.method == "POST":
+        # Create a new user object
+        # Redirect to build_profile
+        user_id = 1
+        return redirect('/build-profile/{}'.format(user_id))
+
+
+def build_profile(request, user_id):
+    if request.method == "GET":
+        context = {}
+        return render(request, 'main/build-profile.html', context)
+    elif request.method == "POST":
+        # Update new user object
+        # Redirect to select strategy
+        return redirect('/user/{}/select-strategy'.format(user_id))
+
+def select_strategy(request, user_id):
+        context = {}
+        return HttpResponse("You've reached select strategy")
